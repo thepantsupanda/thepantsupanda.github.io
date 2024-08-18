0a73f28893b72763ba5427031f01750b6acde988
Panda <panda@Pandas-Air.home>
2024-08-18 15:37:51 -0500

    Complete redesign.

---
+++ script.js
@@ -0,0 +1,72 @@
+// Section for handling background canvas
+
+const canvas = document.querySelector("canvas")
+const context = canvas.getContext("2d")
+
+document.body.onresize = () => {
+    canvas.style.width ='100%'
+    canvas.style.height='100%'
+    canvas.width  = canvas.offsetWidth
+    canvas.height = canvas.offsetHeight
+}
+
+document.body.onresize()
+
+function Circle(x, y, upVelocity, radius) {
+
+    this.x = x
+    this.y = y
+    this.upVelocity = upVelocity
+    this.radius = radius
+
+    this.draw = () => {
+        context.beginPath()
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        context.fillStyle = "rgba(0,0,0,1)"
+        context.fill()
+    }
+
+    this.update = () => {
+        this.y+=this.upVelocity
+        this.draw()
+    }
+
+}
+
+let circleArray = []
+
+function animate() {
+
+    // clears unneeded bubbles from the 
+    for (let i = 0; i < circleArray.length; i++) {
+        if (circleArray[i].y < 0) {
+            circleArray.splice(i, 1)
+        }
+    }
+
+    context.clearRect(0, 0, canvas.width, canvas.height)
+
+    if (Math.random() < 0.01 && circleArray.length < 15) {
+        let radius = (Math.random() * 10) + 1
+        let x = Math.random() * canvas.width
+        let y = canvas.height + radius
+        let upVelocity = (Math.random() * -0.3) - 0.3
+        circleArray.push(new Circle(x, y, upVelocity, radius))
+    } 
+
+    for (let i = 0; i < circleArray.length; i++) {
+        circleArray[i].update()
+    }
+
+    requestAnimationFrame(animate)
+
+}
+
+animate()
+
+// Section for handling image loading stuff
+
+const imageTag = document.querySelector("#sideimg");
+const imageSrc = imageTag.getAttribute("src");
+
+imageTag.setAttribute("src", imageSrc + "?" + Math.random())
--- scripts.js
+++
@@ -1,5 +0,0 @@
-// Done so that the gif reloads every time the page is refreshed.
-let imageTag = document.querySelector("#sideImg");
-let imageSrc = imageTag.getAttribute("src");
-
-imageTag.setAttribute("src", imageSrc + "?" + Math.random())
